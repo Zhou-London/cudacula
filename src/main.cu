c788ef8312ad41e7ee783bdcc723b5c0a5335c33
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-10-12 23:30:44 +0100

    Changes test.

--- src/main.cu
+++ src/main.cu
@@ -1,13 +1,48 @@
+#include <cstddef>
+#include <iostream>
+#include <ranges>
 
-#include <cstdio>
-
-__global__ void helloCUDA(){
-    printf("Hello CUDA\n");
+__global__ void vectorAdd(const float* A, const float* B, float* C, int N) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < N) C[i] = A[i] + B[i];
 }
 
-int main(){
+int main() {
+    int N = 16;
+    size_t size = N * sizeof(float);
 
-    helloCUDA<<<1,5>>>();
-    cudaDeviceSynchronize();
+    float* h_A = new float[N];
+    float* h_B = new float[N];
+    float* h_C = new float[N];
+
+    for (int i : std::ranges::views::iota(0, N)) {
+        h_A[i] = i * 1.0f;
+        h_B[i] = (N - i) * 1.0f;
+    }
+
+    float *d_A, *d_B, *d_C;
+    cudaMalloc(&d_A, size);
+    cudaMalloc(&d_B, size);
+    cudaMalloc(&d_C, size);
+
+    cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
+
+    int threadsPerBlock = 8;
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+    vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, N);
+
+    cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
+
+    for (int i = 0; i < N; ++i)
+        std::cout << h_A[i] << " + " << h_B[i] << " = " << h_C[i] << std::endl;
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+    delete[] h_A;
+    delete[] h_B;
+    delete[] h_C;
+
     return 0;
 }
