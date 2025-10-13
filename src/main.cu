949e27f8aa5927b068a94c05ac8ce8123d526a7f
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-10-13 22:48:02 +0100

    Updates test code.

--- src/main.cu
+++ src/main.cu
@@ -1,41 +1,73 @@
-#include <cstddef>
-#include <iostream>
-#include <ranges>
 
-__global__ void vectorAdd(const float* A, const float* B, float* C, int N) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < N) C[i] = A[i] + B[i];
+#include <iostream>
+
+#define TILE_SIZE 16
+
+__global__ void matMulTiled(float* A, float* B, float* C, int M, int N, int K) {
+    __shared__ float As[TILE_SIZE][TILE_SIZE];
+    __shared__ float Bs[TILE_SIZE][TILE_SIZE];
+
+    int row = blockIdx.y * TILE_SIZE + threadIdx.y;
+    int col = blockIdx.x * TILE_SIZE + threadIdx.x;
+
+    float sum = 0.0f;
+
+    for (int t = 0; t < (K + TILE_SIZE - 1) / TILE_SIZE; t++) {
+        if (row < M && t * TILE_SIZE + threadIdx.x < K)
+            As[threadIdx.y][threadIdx.x] =
+                A[row * K + t * TILE_SIZE + threadIdx.x];
+        else
+            As[threadIdx.y][threadIdx.x] = 0.0f;
+
+        if (col < N && t * TILE_SIZE + threadIdx.y < K)
+            Bs[threadIdx.y][threadIdx.x] =
+                B[(t * TILE_SIZE + threadIdx.y) * N + col];
+        else
+            Bs[threadIdx.y][threadIdx.x] = 0.0f;
+
+        __syncthreads();
+
+        for (int k = 0; k < TILE_SIZE; ++k)
+            sum += As[threadIdx.y][k] * Bs[k][threadIdx.x];
+
+        __syncthreads();
+    }
+
+    if (row < M && col < N) C[row * N + col] = sum;
 }
 
 int main() {
-    int N = 16;
-    size_t size = N * sizeof(float);
+    int M = 64, N = 64, K = 64;
+    size_t sizeA = M * K * sizeof(float);
+    size_t sizeB = K * N * sizeof(float);
+    size_t sizeC = M * N * sizeof(float);
 
-    float* h_A = new float[N];
-    float* h_B = new float[N];
-    float* h_C = new float[N];
+    float* h_A = new float[M * K];
+    float* h_B = new float[K * N];
+    float* h_C = new float[M * N];
 
-    for (const auto& i : std::ranges::views::iota(0, N)) {
-        h_A[i] = i * 1.0f;
-        h_B[i] = (N - i) * 1.0f;
-    }
+    for (int i = 0; i < M * K; ++i) h_A[i] = 1.0f;
+    for (int i = 0; i < K * N; ++i) h_B[i] = 2.0f;
 
     float *d_A, *d_B, *d_C;
-    cudaMalloc(&d_A, size);
-    cudaMalloc(&d_B, size);
-    cudaMalloc(&d_C, size);
+    cudaMalloc(&d_A, sizeA);
+    cudaMalloc(&d_B, sizeB);
+    cudaMalloc(&d_C, sizeC);
 
-    cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_A, h_A, sizeA, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, sizeB, cudaMemcpyHostToDevice);
 
-    int threadsPerBlock = 8;
-    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
-    vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, N);
+    dim3 block(TILE_SIZE, TILE_SIZE);
+    dim3 grid((N + TILE_SIZE - 1) / TILE_SIZE, (M + TILE_SIZE - 1) / TILE_SIZE);
 
-    cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
+    matMulTiled<<<grid, block>>>(d_A, d_B, d_C, M, N, K);
 
-    for (int i = 0; i < N; ++i)
-        std::cout << h_A[i] << " + " << h_B[i] << " = " << h_C[i] << std::endl;
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(h_C, d_C, sizeC, cudaMemcpyDeviceToHost);
+
+    std::cout << "C[0] = " << h_C[0] << std::endl;
+    std::cout << "C[last] = " << h_C[M * N - 1] << std::endl;
 
     cudaFree(d_A);
     cudaFree(d_B);
@@ -43,6 +75,8 @@ int main() {
     delete[] h_A;
     delete[] h_B;
     delete[] h_C;
+
+    return 0;
 
     return 0;
 }
--- src/practices/add_vector.cu
+++ src/practices/add_vector.cu
@@ -1,6 +1,4 @@
 
-#include <iostream>
-
 #include "add_vector.h"
 
 __global__ void vectorAdd(const float* A, const float* B, float* C, int N) {
@@ -18,7 +16,14 @@ void Practices::add_vector(int N, int threadsPerBlock, const float* h_A,
     cudaMalloc(&d_C, size);
 
     cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
 
-    std::cout << "hello\n";
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+    vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, N);
+
+    cudaMemcpy((void*)h_C, d_C, size, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
 }
